f6a747c5ff52675e2800ec42676202ecfe091d14
Kiran <129351284+Kireyy01@users.noreply.github.com>
2025-03-10 00:04:49 +0545

    Update MyOrders.jsx

--- frontend/src/pages/myOrders/MyOrders.jsx
+++ frontend/src/pages/myOrders/MyOrders.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import './myOrders.css'
+import './MyOrders.css'
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
 import { assets } from '../../assets/assets';
